3c41438d702cbe347475305996ba9cb26252b57c
ummyeahumm <wtfuknowbtw@gmail.com>
2026-05-19 00:53:55 +1200

    fix: clean metro config

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,3 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const config = getDefaultConfig(__dirname);
-module.exports = config;
+const { getDefaultConfig } = require('expo/metro-config');
 
-
-const config = getDefaultConfig(__dirname);
-
-// Enable package exports resolution for ESM-only packages (e.g. make-plural used by i18n-js)
-config.resolver.unstable_enablePackageExports = true;
-
-module.exports = withNativeWind(config, { input: "./global.css" });
+module.exports = getDefaultConfig(__dirname);
